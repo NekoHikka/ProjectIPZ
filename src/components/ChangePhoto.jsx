ba77d496797400d04de589a4bbb064e16321b46a
Maryna32 <marinaburda053@gmail.com>
2025-05-20 16:43:38 +0300

    creating new component, modal window for uploading photo

---
+++ src/components/ChangePhoto.jsx
@@ -0,0 +1,64 @@
+import CancelBtn from "../assets/images/buttons/cancel-button.svg";
+import UploadBtn from "../assets/images/CloudUpload.png";
+
+const ChangePhoto = ({
+  onClose,
+  handleFileClick,
+  handleFileChange,
+  fileInputRef,
+  handleUploadPhoto,
+  uploadingPhoto,
+  selectedFile,
+}) => {
+  return (
+    <div className="modal-backdrop">
+      <div className="modal-change-container">
+        <div className="modal-header">
+          <h1>Завантажте ваше зображення</h1>
+          <img
+            src={CancelBtn}
+            alt="кнопка закриття"
+            onClick={onClose}
+            style={{ cursor: "pointer" }}
+          />
+        </div>
+        <div className="content-block">
+          <img src={UploadBtn} alt="кнопка завантаження файлу" />
+          {selectedFile ? (
+            <p className="selected-file-name">Вибрано: {selectedFile.name}</p>
+          ) : (
+            <>
+              <h2>Перетягніть фото сюди</h2>
+              <p>Підтримуваний формат: PNG, JPG</p>
+              <p>Або</p>
+            </>
+          )}
+          <a href="#" onClick={handleFileClick}>
+            {selectedFile ? "Вибрати інший файл" : "Шукати файл"}
+          </a>
+          <input
+            type="file"
+            ref={fileInputRef}
+            accept="image/png, image/jpeg"
+            style={{ display: "none" }}
+            onChange={handleFileChange}
+          />
+        </div>
+        <div className="modal-buttons">
+          <button className="secondaryBtn" onClick={onClose}>
+            Відмінити
+          </button>
+          <button
+            className="primaryBtn"
+            onClick={handleUploadPhoto}
+            disabled={uploadingPhoto || !selectedFile}
+          >
+            {uploadingPhoto ? "Завантаження..." : "Завантажити"}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChangePhoto;
