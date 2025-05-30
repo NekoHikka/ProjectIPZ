fec5874d0421f39a2254d054f2b71ef7f756ecee
Maryna32 <marinaburda053@gmail.com>
2025-05-30 14:49:10 +0300

    adding token

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,3 +5,11 @@ const productionUrl = "http://127.0.0.1:8000/api";
 export const customFetch = axios.create({
   baseURL: productionUrl,
 });
+
+customFetch.interceptors.request.use((config) => {
+  const token = localStorage.getItem("access");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
