9251fc0c032dd99ce0471a980e8144689fc46126
Maryna32 <marinaburda053@gmail.com>
2025-05-30 15:23:30 +0300

    fixing bug

--- src/data/navlinks.js
+++ src/data/navlinks.js
@@ -7,17 +7,17 @@ import ordersActive from "../assets/images/navbar/ordersActive.svg";
 import restorantsActive from "../assets/images/navbar/restorantsActive.svg";
 
 export const links = [
-  { id: 1, url: "menu", text: "Меню", img: menu, activeImg: menuActive },
+  { id: 1, url: "/menu", text: "Меню", img: menu, activeImg: menuActive },
   {
     id: 2,
-    url: "orders",
+    url: "/orders",
     text: "Замовлення",
     img: orders,
     activeImg: ordersActive,
   },
   {
     id: 3,
-    url: "restorants",
+    url: "/restorants",
     text: "Заклади",
     img: restorants,
     activeImg: restorantsActive,
